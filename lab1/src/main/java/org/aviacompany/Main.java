fb3a190a8cb590a3945fb4624a49db2826231051
b0r3d <bogd.reschetko2017@gmail.com>
2025-03-22 18:47:04 +0200

    added logging configuration

--- lab1/src/main/java/org/aviacompany/Main.java
+++ lab1/src/main/java/org/aviacompany/Main.java
@@ -1,5 +1,7 @@
 package org.aviacompany;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.ThreadContext; //бібліотека буде писати в чому суть проблеми, а не просто вказувать місце де вона є
 
 public class Main {
     public static void main(String[] args) {
